c8632b7a0d315b234eabb737a9c7d7b2dd5ed3c0
Miguel Cortes <miguelangel-yektik@hotmail.com>
2021-02-08 02:00:54 -0600

    primer commit

---
+++ src/assets/scripts/loti.js
@@ -0,0 +1,9 @@
+
+var animacion = bodymovin.loadAnimation({
+    container: document.getElementById('logo'), // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+    path: '_data/yektik.json'
+    //path: 'https://assets9.lottiefiles.com/datafiles/gUENLc1262ccKIO/data.json' // Required
+  })    
